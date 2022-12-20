03045d54fc8bcd4277f06558dbd4779f7d060c4b
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-12-20 20:54:43 +0900

    Feat: 기초 세팅으로 Recoil, ReactQuery 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import { RecoilRoot } from 'recoil'
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
