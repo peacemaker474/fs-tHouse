5a481245ec88eaec0acc70125d372181569d75e2
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-01-11 22:56:14 +0900

    Feat: Header 및 GNB 레이아웃 기준 잡기

---
+++ components/gnb/organisms/GNB.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+function GNB () {
+  return (
+    <GNBWrapper>
+
+    </GNBWrapper>
+  );
+}
+
+export default GNB;
+
+const GNBWrapper = styled.header`
+  max-width: 1265px;
+  width: 100%;
+  height: 81px;
+  margin: 0 auto;
+  background-color: blue;
+`;
